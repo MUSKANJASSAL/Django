0cba968aa5ecb5b12a8623f6c3f1c07d929ae820
Muskan Jassal <32767876+MUSKANJASSAL@users.noreply.github.com>
2021-01-18 20:04:33 +0530

    Delete RESTBasics directory

--- RESTBasics/api_basic/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Article
-
-# Register your models here.
-# As we want to access the model through administration panel
-
-admin.site.register(Article)
